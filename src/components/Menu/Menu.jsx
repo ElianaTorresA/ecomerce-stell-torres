d96d3e1e8526a235f7982129c76282daacf30c24
elianatorres <elianita-torres@hotmail.com>
2022-07-05 19:35:19 -0500

    se agrega menu

---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,13 @@
+function Menu () {
+    return(
+      <div>
+        <h2>Stell casa de modas</h2>
+        <nav>
+          <p>Hombres</p>
+          <p>Mujeres</p>
+          <p>Niños</p>
+        </nav>
+      </div>
+    )
+  }
+export default Menu
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,33 @@
+
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from "react-bootstrap/Nav"
+import Container from "react-bootstrap/Container"
+import NavDropdown from "react-bootstrap/NavDropdown"
+
+
+
+const NavBar = () => {
+  return (
+    <Navbar bg="light" expand="lg">
+        <Container>
+        <Navbar.Brand href="#home">Stell Casa de modas</Navbar.Brand>
+        <Navbar.Collapse id="basic-navbar-nav">
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Nav className="me-auto">
+            <Nav.Link href="#home">Sobre nosotros</Nav.Link>
+            <Nav.Link href="#link">Donde estamos</Nav.Link>
+            <NavDropdown title="Opciones" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Hombres</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Mujeres</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Niños</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">A tu medida</NavDropdown.Item>
+            </NavDropdown>
+            </Nav>
+        </Navbar.Collapse>
+        </Container>
+    </Navbar>
+  )
+}
+
+export default NavBar
